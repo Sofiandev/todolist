38dff989496de19b6962127b18253961548de9f5
SofianAcDev <sofian.achbabi@gmail.com>
2023-03-20 21:18:13 +0100

    To Do List

---
+++ app.js
@@ -0,0 +1,33 @@
+//creer un form pour taper les todos
+
+//ajouter sur le dom ce qui est tapé dans l'input à la validation du form
+
+//supprimer un todo losque l'on clique dessus
+
+//stocker dans le local storage la liste
+
+//consulter le local storage au lancement de l'application pour rajouter les todos
+
+const todo = document.querySelector("#todo");
+const todolist = document.querySelector(".todolist");
+const form = document.querySelector("form");
+const error = document.querySelector("span");
+const removetodo = document.querySelector("ul");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (todo.value.length <= 3) {
+    error.style.visibility = "visible";
+  } else {
+    todolist.innerHTML += `<li>${todo.value}</li>`;
+    error.style.visibility = "hidden";
+    todo.value = "";
+  }
+});
+
+
+removetodo.addEventListener('click',(e)=>{
+    e.target.classList.add ="checked";
+    // e.target.remove();
+console.log(e.target)
+})
